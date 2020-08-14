764a1f7c8c12e4ebdd8065a98a5c4672cdfc4fb2
NOYB <ayberkbora.acarbabacan@gmail.com>
2020-08-14 12:52:36 +0000

    fix typo

--- commands/helpgenerators.js
+++ commands/helpgenerators.js
@@ -8,8 +8,8 @@ const embed = new Discord.MessageEmbed()
     .setFooter(
       'Other Generators\n' +
       "mc!datapack-1.13 - Check out U1timateJ7's 1.13.x Data Pack Generator.\n" +
-      "mc!datapack-1.16 - Check out U1timateJ7's 1.13.x Data Pack Generator.\n" +
-      "mc!fabric - Check out Goldorions's Fabric Generator.\n" +
+      "mc!datapack-1.16 - Check out U1timateJ7's 1.16.x Data Pack Generator.\n" +
+      "mc!fabric - Check out Goldorion's Fabric Generator.\n" +
       "mc!forge-1.12 - Check out Klemen's 1.12.2 Forge Generator.\n" +
       "mc!spigot - Check out Goldorion's Spigot Plugin Generator.\n"
       );
