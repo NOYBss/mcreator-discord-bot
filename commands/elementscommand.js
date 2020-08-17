beda7c1e238764a0b9a7ca8ad83f2905776a5c8b
NOYB <ayberkbora.acarbabacan@gmail.com>
2020-08-17 13:01:37 +0000

    Fix command element command

--- commands/elementscommand.js
+++ commands/elementscommand.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const embed = new Discord.MessageEmbed()
     .setTitle('How to Make a Command')
-    .setURL('https://mcreator.net/wiki/how-make-command')
+    .setURL('https://mcreator.net/wiki/making-command')
     .setColor(0xffffff)
     .setThumbnail('https://media.discordapp.net/attachments/674675708374679603/738785529856000060/logo-mcreator.jpg')
     .setDescription('Command is a set of text that triggers action when player types it into chat.');
