6b7a124bc5200519806a10802e3fcd65ac1d0616
NOYB <ayberkbora.acarbabacan@gmail.com>
2020-08-30 06:40:45 +0000

    some coldfixes

--- commands/elementsenchantment.js
+++ commands/elementsenchantment.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 
 const embed = new Discord.MessageEmbed()
-    .setTitle('How to Make a Enhcantment')
-    .setURL('https://mcreator.net/wiki/how-make-dimension')
+    .setTitle('How to Make a Enchantment')
+    .setURL('https://mcreator.net/wiki/how-make-enchantment')
     .setColor(0xffffff)
     .setThumbnail('https://media.discordapp.net/attachments/674675708374679603/738785529856000060/logo-mcreator.jpg')
     .setDescription('Enchantment is a modifier that adds special abilities to items when applied.');
--- commands/elementsloottable.js
+++ commands/elementsloottable.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const embed = new Discord.MessageEmbed()
     .setTitle('How to Make a Loot Table')
-    .setURL('https://mcreator.net/wiki/how-make-lot-table')
+    .setURL('https://mcreator.net/wiki/how-make-loot-table')
     .setColor(0xffffff)
     .setThumbnail('https://media.discordapp.net/attachments/674675708374679603/738785529856000060/logo-mcreator.jpg')
     .setDescription('Loot table represents what items should be generated naturally in containers.');
