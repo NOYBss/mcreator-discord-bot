46f4b0cc58eaf3f47a5e2f6a716ac4424c09d42f
NOYB <ayberkbora.acarbabacan@gmail.com>
2020-09-07 12:41:27 +0000

    v1.0.10 hotfix

--- commands/mcreator.js
+++ commands/mcreator.js
@@ -6,7 +6,7 @@ const embed = new Discord.MessageEmbed()
     .setColor(0xffffff)
     .setThumbnail('https://media.discordapp.net/attachments/674675708374679603/738785529856000060/logo-mcreator.jpg')
     .addField('About', 'MCreator is open source software used to make Minecraft Java Edition mods, Bedrock Edition Add-Ons, and data packs using an intuitive easy-to-learn interface or with an integrated code editor. It is used worldwide by Minecraft players, mod developers, for education and by STEM workshops.')
-    .addField('License', '[GNU Lesser General Public License v3.0](https://www.gnu.org/licenses/lgpl-3.0.en.html)')
+    .addField('License', '[GNU General Public License v3.0](http://www.gnu.org/licenses/gpl-3.0.en.html)')
     .addField('Issue Tracker', '[GitHub Issues](https://github.com/MCreator/MCreator/issues)')
     .addField('Source Code', '[GitHub Repository](https://github.com/MCreator/MCreator)')
     ;
